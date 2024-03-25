a316f74e698710266b26fe8a8c95d9efc78f0aed
Pasquale, Pinto <p.pinto@avanade.com>
2024-03-25 09:56:08 +0100

    chore: display messages on execution

--- customize.sh
+++ customize.sh
@@ -1,5 +1,33 @@
 #!/system/bin/sh
+MODDIR=${0%/*}
 
-chmod 777 "$MODPATH/service.sh"
+# detect active slot
+SLOT=$(getprop ro.boot.slot_suffix)
+# detect device model
+MODEL=$(getprop ro.product.model)
+# detect current module name
+MODNAME=$(grep "^id=" module.prop|cut -d'=' -f2)
+# find model related abl from current module directory in magisk modules folder
+FILE=$(find /data/adb ${MODNAME} | grep ${MODEL}/abl.img | head -n1)
+echo
+echo
 
-`$MODPATH/service.sh`
+if [ -f $FILE ]; then
+  echo >$MODDIR/disable
+  
+  echo "  Good News!"
+  echo "    detected model ${MODEL}, active slot is ${SLOT}..."
+  echo
+  echo "    let's patch it!"
+  $`dd if=$FILE of=/dev/block/by-name/abl${SLOT} ;`;
+else
+  echo >$MODDIR/delete
+  
+  echo "  Sorry: $MODEL is not supported. No action executed"
+  echo
+  echo "    Module will self-delete on next boot"
+fi
+
+echo
+echo
+echo "Enjoy!"
--- service.sh
+++ service.sh
@@ -1,28 +1,33 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
-MODEL=$(getprop ro.product.model)
+# detect active slot
 SLOT=$(getprop ro.boot.slot_suffix)
+# detect device model
+MODEL=$(getprop ro.product.model)
+# detect current module name
+MODNAME=$(grep "^id=" module.prop|cut -d'=' -f2)
+# find model related abl from current module directory in magisk modules folder
+FILE=$(find /data/adb ${MODNAME} | grep ${MODEL}/abl.img | head -n1)
+echo
+echo
 
-FILE=$MODDIR/$MODEL/abl.img
-
-if [ -f "$FILE" ]; then
+if [ -f $FILE ]; then
   echo >$MODDIR/disable
   
-  echo "Good News!"
-  echo "detected model $MODEL, active slot is $SLOT..."
+  echo "  Good News!"
+  echo "    detected model ${MODEL}, active slot is ${SLOT}..."
   echo
-  echo "let's patch it!"
-  $`dd if=$FILE of=/dev/block/by-name/abl$SLOT ;`;
-  echo
+  echo "    let's patch it!"
+  $`dd if=$FILE of=/dev/block/by-name/abl${SLOT} ;`;
 else
   echo >$MODDIR/delete
   
-  echo "Sorry: $MODEL is not supported. No action executed"
+  echo "  Sorry: $MODEL is not supported. No action executed"
   echo
-  echo "Module will self-delete on next boot"
-  echo
+  echo "    Module will self-delete on next boot"
 fi
 
+echo
 echo
 echo "Enjoy!"
